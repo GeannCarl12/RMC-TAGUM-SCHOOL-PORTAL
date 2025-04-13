78a8009ed88ef13e65b94bd32fa60ca519932d0a
GeannCarl12 <nobodygaming198@gmail.com>
2025-04-13 16:28:32 +0800

    Update navigationforstudent.js

--- navigationforstudent.js
+++ navigationforstudent.js
@@ -732,7 +732,7 @@ if (schoolNewsButton) {
         console.log('Logout button clicked');
         if (loadingScreen) loadingScreen.classList.add('active');
         setTimeout(() => {
-            window.location.href = 'https://geanncarl12.github.io/RMC-TAGUM-SCHOOL-PORTAL/';
+            window.location.href = 'index.html';
         }, 5000);
     }
     
